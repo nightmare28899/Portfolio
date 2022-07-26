3d532050c48c698dfe07267421ea32bc70f44d7a
Kevin López González <86860306+Nightmare289@users.noreply.github.com>
2022-07-26 16:59:51 -0500

    updasted

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,110 +1,98 @@
-import React from 'react'
-import './experience.css'
-import { BsPatchCheckFill } from 'react-icons/bs'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 export default function experience() {
+  const experiencesFronted = [
+    {
+      title: "HTML",
+      level: "Experienced",
+    },
+    {
+      title: "CSS",
+      level: "Intermediate",
+    },
+    {
+      title: "JavaScript",
+      level: "Intermediate",
+    },
+    {
+      title: "React",
+      level: "Basic",
+    },
+    {
+      title: "Angular",
+      level: "Intermediate",
+    },
+    {
+      title: "TailwindCSS",
+      level: "Intermediate",
+    },
+  ];
+
+  const listItemsFronted = experiencesFronted.map((experience) => (
+    <article className="experience_details">
+      <BsPatchCheckFill className="experience_details" />
+      <div>
+        <h4>{experience.title}</h4>
+        <small className="text-light">{experience.level}</small>
+      </div>
+    </article>
+  ));
+
+  const experiencesBackend = [
+    {
+      title: "NodeJS",
+      level: "Basic",
+    },
+    {
+      title: "ExpressJS",
+      level: "Basic",
+    },
+    {
+      title: "MySQL",
+      level: "Basic",
+    },
+    {
+      title: "PostgreSQL",
+      level: "Basic",
+    },
+    {
+      title: "Laravel",
+      level: "Intermediate",
+    },
+    {
+      title: "PHP",
+      level: "Intermediate",
+    },
+  ];
+
+  const listItemsBackend = experiencesBackend.map((experience) => (
+    <article className="experience_details">
+      <BsPatchCheckFill className="experience_details" />
+      <div>
+        <h4>{experience.title}</h4>
+        <small className="text-light">{experience.level}</small>
+      </div>
+    </article>
+  ));
+
   return (
-    <section id='experience'>
+    <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
 
       <div className="container experience_container">
         <div className="experience_frontend">
           <h3>Frontend Development</h3>
-          <div className="experience_content">
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>CSS</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>JavaScript</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>Bootstrap</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>Tailwind</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>React</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-          </div>
+          <div className="experience_content">{listItemsFronted}</div>
         </div>
         {/* END OF FRONTEND */}
         <div className="experience_backend">
           <h3>Backend Development</h3>
-          <div className="experience_content">
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>Node JS</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>MongoDB</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>PHP</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>MySQL</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>Python</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience_details'>
-              <BsPatchCheckFill className='experience_details' />
-              <div>
-                <h4>Laravel</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-          </div>
+          <div className="experience_content">{listItemsBackend}</div>
         </div>
       </div>
     </section>
-  )
+  );
 }
