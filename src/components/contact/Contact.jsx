f36990c1adcbe1610ef76f67c5760ada7acc2d43
nightmare28899 <46544389+nightmare28899@users.noreply.github.com>
2022-07-28 01:16:41 -0500

    update
    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,57 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 export default function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_y1w723w', 'TEMPLATE_ID', form.current, 'USER_ID')
+  }
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>nightmare28899@gmail.com</h5>
+            <a href="mailto:nightmare28899@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>nightmare28899</h5>
+            <a href="https://m.me/Kevin28899" target="_blank">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp />
+            <h4>WhatsAPP</h4>
+            <h5>+52 4436397414</h5>
+            <a href="https://api.whatsapp.com/send?phone=4436397414" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF THE CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" id="" rows="7" placeholder="Your Message" required ></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,10 +5,6 @@ import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
-
 export default function Testimonials() {
   const testimonials = [
     {
@@ -37,33 +33,26 @@ export default function Testimonials() {
     },
   ];
 
+  const testimonialList = testimonials.map((item, index) => (
+    <div key={index} className="testimonial">
+      <div className="client_avatar">
+        <img src={item.avatar} alt="" />
+      </div>
+      <h5 className="client_name">{item.name}</h5>
+      <small className="client_review">{item.review}</small>
+    </div>
+
+  ));
+
   return (
     <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <Swiper
-        className="container testimonials-container"
-        spaceBetween={50}
-        slidesPerView={3}
-        centeredSlides
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-      >
-        {testimonials.avatar
-          ? testimonials.map(({ avatar, name, review }, index) => {
-              return (
-                <SwiperSlide key={index} className="testimonial">
-                  <div className="testimonial-avatar">
-                    <img src={avatar} alt="" />
-                  </div>
-                  <h5 className="client_name">{name}</h5>
-                  <small className="client_review">{review}</small>
-                </SwiperSlide>
-              );
-            })
-          : null}
-      </Swiper>
+      <div
+        className="container testimonials-container">
+        {testimonialList}
+      </div>
     </section>
   );
 }
